4051472fb02631239f538184db35fdd3fe281931
Lamine <lamine@mbp-de-lamine.lan>
2021-07-09 16:16:47 +0200

    first commit

---
+++ routes/offer.js
@@ -0,0 +1,134 @@
+const express = require("express");
+const router = express();
+const isAuthenticated = require("../middlewares/isAuthenticated");
+const cloudinary = require("cloudinary").v2;
+
+// import des models
+const User = require("../models/User");
+const Offer = require("../models/Offer");
+
+// route pour publier les annonces
+router.post("/offer/publish", isAuthenticated, async (req, res) => {
+  try {
+    //   console.log(req.user);
+
+    // Destructuring
+    const { title, description, price, condition, city, brand, size, color } =
+      req.fields;
+
+    // Déclaration d'une nouvelle offre newOffer
+    const newOffer = new Offer({
+      product_name: title,
+      product_description: description,
+      product_price: price,
+      product_details: [
+        {
+          MARQUE: brand,
+        },
+        {
+          TAILLE: size,
+        },
+        {
+          ÉTAT: condition,
+        },
+        {
+          COULEUR: color,
+        },
+        {
+          EMPLACEMENT: city,
+        },
+      ],
+      owner: req.user,
+    });
+
+    // Uploader l'image presente dans cloudinary
+    const result = await cloudinary.uploader.upload(req.files.picture.path, {
+      folder: `/vinted/offer/${newOffer._id}`,
+    });
+
+    // Ajouter le resultat de l'upload dans newOffer
+    newOffer.product_image = result;
+
+    // Sauvgarder newOffer
+    await newOffer.save();
+
+    // reponse du client newOffer
+    res.json(newOffer);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Route pour recevoir des offres
+router.get("/offers", async (req, res) => {
+  try {
+    const filters = {};
+    let sort = {};
+
+    if (req.query.title) {
+      filters.product_name = new RegExp(req.query.title, "i");
+      // ajoute une clé product_name à l'objet filters
+      // cette clé a pour valeur new RegExp(req.query.title, "i")
+      // {
+      //   product_name: new RegExp(req.query.title, "i");
+      // }
+    }
+
+    if (req.query.priceMin) {
+      filters.product_price = { $gte: Number(req.query.priceMin) };
+    }
+
+    if (req.query.priceMax) {
+      if (filters.product_price) {
+        filters.product_price.$lte = Number(req.query.priceMax);
+      } else {
+        filters.product_price = { $lte: Number(req.query.priceMax) };
+      }
+    }
+
+    if (req.query.sort === "price-asc") {
+      sort = { product_price: 1 };
+    } else if (req.query.sort === "price-desc") {
+      sort = { product_price: -1 };
+    }
+
+    let page;
+    const limit = Number(req.query.limit);
+
+    if (Number(req.query.page < 1)) {
+      page = 1;
+    } else {
+      page = Number(req.query.page);
+    }
+
+    const offers = await Offer.find(filters)
+      .populate({ path: "owner", select: "account" })
+      .sort(sort)
+      .skip((page - 1) * limit)
+      .limit(limit);
+
+    const count = await Offer.countDocuments(filters); // renvoyer le nombre de documents qui matchent avec filters
+
+    res.status(200).json({
+      count: count,
+      offers: offers,
+    });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+// Route pour recevoir l'offre en fonction de l'ID
+router.get("/offer/:id", async (req, res) => {
+  try {
+    const offer = await Offer.findById(req.params.id).populate({
+      path: "owner",
+      select: "account",
+    });
+    res.status(200).json(offer);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = router;
